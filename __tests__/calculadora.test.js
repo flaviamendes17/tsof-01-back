5c8d9ed2d98681914ade4432b91aeddd09643480
Flavia Mendes <flavinharamendes@gmail.com>
2025-08-26 14:30:43 -0300

    feat: create subtrair test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar } from "../src/calculadora.js";
+import { somar, subtrair } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
     test("deve somar dois números corretamente", () => {
@@ -8,5 +8,13 @@ describe("Calculadora", () => {
         const resultado = somar(a, b);
 
         expect(resultado).toBe(12);
+    });
+    test("deve subtrair dois números corretamente", () => {
+        const a = 5;
+        const b = 7;
+
+        const resultado = subtrair(a, b);
+
+        expect(resultado).toBe(-2);
     });
 })
